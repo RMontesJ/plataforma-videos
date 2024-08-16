9cd291fab0730cad43892ab0e582f8817f834ab9
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-08-16 16:38:05 +0200

    Changed design of nav

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,10 @@
 
 $id = $_GET['id_user'];
 
+require_once "../DB/DB_Connection.php";
+$DB = new DB();
+$picture = $DB->catchPicture($id);
+
 ?>
 
 <header>
@@ -11,8 +15,12 @@ $id = $_GET['id_user'];
             <li><a href="../pages/main.php?id_user=<?php echo $id ?>">Inicio</a></li>
             <li><a href="../pages/post.php?id_user=<?php echo $id ?>">Crear video</a></li>
             <li><a href="../pages/profiles.php?id_user=<?php echo $id ?>">Descubrir gente</a></li>
-            <li><a href="../pages/myProfile.php?id_user=<?php echo $id ?>">Mi perfil</a></li>
+            <li></li>
         </ul>
     </nav>
-    <a href="../logic/logic_logout.php"><button>Cerrar sesion</button></a>
+    <div class="profile-logout-box">
+    <a href="../pages/myProfile.php?id_user=<?php echo $id ?>"><img src="../profile-pictures/<?php echo $picture; ?>" alt="My profile picture" style="width:40px;height:40px;"></a>
+    <a href="../logic/logic_logout.php"><img src="../icons/logout_icon.svg" alt="Logout" style="width:40px;height:40px;"></a>
+    </div>
+    
 </header>
