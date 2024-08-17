3b360b2b9a5c9a66db32993ed8f0b482ac9fa80f
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-08-17 13:35:28 +0200

    Revert "Trying to prevent sql inyection"
    This reverts commit 55457f92291c92484bbf54bcc2315d4682e86848.

--- valid/changeProfile.js
+++ valid/changeProfile.js
@@ -15,7 +15,7 @@ let corregirName = document.getElementById('corregirName');
 
 let validar = true;
 
-if(name.value.trim() === "" || !/^[a-zA-Z\s]{1,25}[^'"]$/.test(name.value)){
+if(name.value.trim() === "" || !/^[a-zA-Z\s]{1,25}$/.test(name.value)){
     name = document.getElementById('name').style.border = "1px red solid";
     corregirName.style.color = 'red';
     corregirName.innerHTML = "El nombre tiene que tener entre 1 y 25 caracteres, sin caracteres especiales";
